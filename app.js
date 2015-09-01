977b0cf7fd6a837afea00f66ea3e9af9b68b7b42
Rodrigo López Dato <rodrigo@auth0.com>
2015-09-01 19:13:48 -0300

    Initial

---
+++ app.js
@@ -0,0 +1,18 @@
+//==========
+//===EH LEE LA DOCUMENTACIÓN ACA=====
+//==http://expressjs.com/guide/routing.html===
+//==Para más detalles de la API MIRA ACA===
+//==http://expressjs.com/4x/api.html
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
